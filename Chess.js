aae456a9f68d3053e0f73051f0e0191ecfe61f78
Connor <connor.m.dodge@gmail.com>
2016-03-03 13:58:38 -0700

    Pawns on board commit

--- Chess.js
+++ Chess.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var settings = {
+	var global = {
 		whiteMoveFlag: true,
 		gameBoardArr: [],
 		threatenedByWhiteArr: [],
@@ -8,32 +8,98 @@ $(document).ready(function(){
 		redoArr: [],
 	}
 
-	makeGameBoard(settings.gameBoardArr, 8, 8)
+	var imagePaths = {
+		whitePawn:'<img src="./Assets/whitePawn.png">',
+		whiteRook:'<img src="./Assets/whiteRook.png">',
+		whiteBishop:'<img src="./Assets/whiteBishop.png">',
+		whiteKnight:'<img src="./Assets/whiteKnight.png">',
+		whiteQueen:'<img src="./Assets/whiteQueen.png">',
+		wKing:'<img src="./Assets/whiteKing.png">',
+		blackPawn:'<img src="./Assets/blackPawn.png">',
+		blackRook:'<img src="./Assets/blackRook.png">',
+		blackBishop:'<img src="./Assets/blackBishop.png">',
+		blackKnight:'<img src="./Assets/blackKnight.png">',
+		blackQueen:'<img src="./Assets/blackQueen.png">',
+		blackKing:'<img src="./Assets/blackKing.png">',
+	}
 
-//===============================FUNCTIONS====================================
+	global.gameBoardArr = makeGameBoard(global.gameBoardArr, 8, 8);
+	global.gameBoardArr = initializePieces(global.gameBoardArr, 8, 8);
+	console.log(global.gameBoardArr);
+
+
+//======================================FUNCTIONS=============================================
 	function makeGameBoard(gameBoardArr, height, width){
-	
-	var flag;
-		for(var i = 0; i < height; i++){
-			var row = [];
-			if(i%2 == 0){
-				flag = true;
-			}else{
-				flag = false;
-			}
-			for(var j = 0; j < width; j++){
-				if(flag){
-					$("#gameBoard").append('<div class="blackSquare"></div>');
-					row.push({'position': [i,j]})
-					flag = false;
-				} else{
-					$("#gameBoard").append('<div class="whiteSquare"></div>');
+		var flag;
+			for(var i = 0; i < height; i++){
+				var row = [];
+				if(i%2 == 0){
 					flag = true;
+				}else{
+					flag = false;
 				}
+				for(var j = 0; j < width; j++){
+					if(flag){
+						$("#gameBoard").append('<div class="blackSquare"></div>');
+						row.push({'position': i.toString()+j.toString()});
+						flag = false;
+					} else{
+						$("#gameBoard").append('<div class="whiteSquare"></div>');
+						row.push({'position': i.toString()+j.toString()});
+						flag = true;
+					}
 
-				$("#gameBoard div:last-child").attr("id",i.toString()+j.toString());
+					$("#gameBoard div:last-child").attr("id",i.toString()+j.toString());
+				}
+			gameBoardArr.push(row);
+			}
+			return gameBoardArr;
+	}
+
+	function initializePieces(gameBoardArr, height, width){
+		console.log("initializePieces");
+		for(var i = 0; i < height; i++){
+			for(var j = 0; j < width; j++){
+				if(i === 1){
+					gameBoardArr[i][j].piece = makePiece('black','pawn');
+					var piece =gameBoardArr[i][j].piece;
+					putPieceInSquare(piece,i,j);
+				} else if(i === 6){
+					gameBoardArr[i][j].piece = makePiece('white','pawn');
+					var piece =gameBoardArr[i][j].piece;
+					putPieceInSquare(piece,i,j);
+				} else if(i === 0){
+
+				}
+				
 			}
 		}
-		gameBoardArr.push(row);
+		return gameBoardArr;
 	}
-});
+
+	function makePiece(color, type){
+		console.log("makePiece");
+		var piece = {
+			'color': color,
+			'type': type,
+			'image': '<img src="./Assets/'+color+capitalize(type)+'.png">',
+			'targetSquares': [],
+			// 'image': imagePaths.blackKnight
+		}
+		return piece
+	}
+
+	function capitalize(str){
+		str = str.substring(0,1).toUpperCase() + str.substring(1);
+		return str;
+	}
+
+	function putPieceInSquare(piece,row,col){
+		$("#"+row.toString()+col.toString()).append(piece.image);
+	}
+
+	function emptySquare(row,col){
+		$("#"+row.toString()+col.toString()).text('');
+	}
+
+	});

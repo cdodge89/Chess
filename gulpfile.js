f0d0df70f1a754f89aa1cc9a539e41b727e7a0fa
Connor Dodge <Connor Dodge>
2016-03-28 09:19:23 -0600

    Gulp/Sass final commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,42 +6,68 @@ var gulp = require('gulp'),
   rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps'),
-     del = require('del');
+     del = require('del'),
+    open = require('gulp-open'),
+cleanCss = require('gulp-clean-css');
 
-     gulp.task("concatScripts", function() {
-	    gulp.src([
-	        'dist/bower_components/jquery/dist/jquery.js',
-	        'dist/bower_components/boostrap/dist/js/boostrap.js',
-	        'js/Chess.js'
-	        ])
-	    .pipe(maps.init())
-	    .pipe(concat('app.js'))
-	    .pipe(maps.write('./'))
-	    .pipe(gulp.dest('dist'));
-	});
+ gulp.task("concatScripts", function() {
+   return gulp.src([
+        'dist/bower_components/jquery/dist/jquery.js',
+        'dist/bower_components/boostrap/dist/js/boostrap.js',
+        'js/Chess.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('dist'));
+});
 
-     gulp.task("minifyScripts", ["concatScripts"], function() {
-	  gulp.src("dist/app.js")
-	    .pipe(uglify())
-	    .pipe(rename('app.min.js'))
-	    .pipe(gulp.dest('dist'));
-	});
+ gulp.task("minifyScripts", ["concatScripts"], function() {
+   return gulp.src("dist/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
 
-     gulp.task('compileSass', function() {
-	  gulp.src("src/scss/application.scss")
-	      .pipe(maps.init())
-	      .pipe(sass())
-	      .pipe(rename('main.css'))
-	      .pipe(maps.write('./'))
-	      .pipe(gulp.dest('dist'));
-	});
+ gulp.task('compileSass', function() {
+  return gulp.src("src/scss/application.scss")
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(rename('main.css'))
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('dist'));
+});
 
-     gulp.task('copyViews', function(){
-     	gulp.src('src/views/*html')
-     		.pipe(gulp.dest('dist/views'));
-     });
+ gulp.task('minifyCss', ['compileSass'], function(){
+ 	return gulp.src("dist/main.css")
+    .pipe(cleanCss())
+    .pipe(gulp.dest('dist'));
+ });
 
-     gulp.task('copyIndex', function(){
-     	gulp.src('src/index.html')
-     		.pipe(gulp.dest('dist'));
-     });
+ gulp.task('copyViews', function(){
+ 	return gulp.src('src/views/*.html')
+ 		.pipe(gulp.dest('dist/views'));
+ });
+
+ gulp.task('copyIndex', function(){
+ 	return gulp.src('src/index.html')
+ 		.pipe(gulp.dest('dist'));
+ });
+
+ gulp.task('watchFiles', function() {
+  gulp.watch('scss/**/*.scss', ['compileSass']);
+  gulp.watch('js/Chess.js', ['concatScripts']);
+  gulp.watch('src/index.html', ['copyIndex']);
+});
+
+ gulp.task('serve', ['watchFiles', 'compileSass', 'concatScripts', 'copyIndex', 'copyViews'], function(){
+ 	gulp.src('dist/index.html')
+ 		.pipe(open({app: 'chrome'}));
+ });
+
+ gulp.task('default', ['serve'], function(){
+ 	gulp.watch('scss/**/*.scss', ['serve']);
+	gulp.watch('js/Chess.js', ['serve']);
+	gulp.watch('src/index.html', ['serve']);
+ });
+
+ gulp.task('deploy', ['minifyScripts', 'minifyCss', 'copyIndex', 'copyViews']);
